6d579a010cc187538763b0da936e5f9cd322a4ab
Ninolincy <lincy3akoth@gmail.com>
2024-10-30 20:53:00 +0300

    MRU Algorithm

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 
-""" LRU Caching """
+""" MRU Caching """
 from base_caching import BaseCaching
 
 
-class LRUCache(BaseCaching):
-    """ LRU caching algorithm """
+class MRUCache(BaseCaching):
+    """ MRU caching algorithm """
     def __init__(self):
         """ Initialize cache"""
         super().__init__()
 
     def put(self, key, item):
         """ Add an item in the cache """
-        if key and item:
+        if key is not None and item is not None:
             if key not in self.cache_data:
                 if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
                     most_key = self.cache_data.popitem()
