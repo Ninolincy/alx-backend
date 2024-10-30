8326a60643b4fe15923fb43040e6edd1cccc2ae0
Ninolincy <lincy3akoth@gmail.com>
2024-10-30 20:42:45 +0300

    LIFO Algorithm

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -11,7 +11,7 @@ class LIFOCache(BaseCaching):
 
     def put(self, key, item):
         """ Add an item in the cache """
-        if key and item:
+        if key is not None and item is not None:
             if key not in self.cache_data:
                 if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
                     first_key = self.cache_data.popitem()
