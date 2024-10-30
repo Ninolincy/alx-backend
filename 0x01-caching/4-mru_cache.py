6edf34d3121def61ea686f4e117077ac7f8de290
Ninolincy <lincy3akoth@gmail.com>
2024-10-30 20:50:48 +0300

    MRU Algorithm

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -15,9 +15,9 @@ class LRUCache(BaseCaching):
         if key and item:
             if key not in self.cache_data:
                 if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                    least_key = self.cache_data.popitem()
-                    """discard least recently used item"""
-                    print("DISCARD: {}".format, least_key)
+                    most_key = self.cache_data.popitem()
+                    """discard most recently used item"""
+                    print("DISCARD: {}".format, most_key)
             self.cache_data[key] = item
 
     def get(self, key):
