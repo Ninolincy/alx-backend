92695e6213f3cf27735bcbe88c0006e715aee1d6
Ninolincy <lincy3akoth@gmail.com>
2024-10-30 20:40:22 +0300

    LIFO Algorithm

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" LIFO Caching """
+from base_caching import BaseCaching
+
+class LIFOCache(BaseCaching):
+    """ LIFO caching algorithm """
+    def __init__(self):
+        """ Initialize cache"""
+        super().__init__()
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key and item:
+            if key not in self.cache_data:
+                if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                    first_key = self.cache_data.popitem()
+                    """discard first item LIFO algorithm"""
+                    print("DISCARD: {}".format, first_key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ return the value of key in cache """
+        return self.cache_data.get(key)  
