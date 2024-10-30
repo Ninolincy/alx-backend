dfc978a9387bb1bb97981c05dddb9df564e6a64c
Ninolincy <lincy3akoth@gmail.com>
2024-10-30 20:32:25 +0300

    Basic Cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,12 +5,11 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    
     def put(self, key, item):
         """ assign to dictionary item the value of key"""
         if key and item:
             self.cache_data[key] = item
-    
+
     def get(self, key):
         """ return the value of key in cache """
         return self.cache_data.get(key)
---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" FIFO cache """
+from base_caching import BaseCaching
+
+class FIFOCache(BaseCaching):
+    """ FIFO cache class
+    """
+    def __init__(self) -> None:
+        """ Constructor method to initialize FIFO"""
+        super().__init__()
+    
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                """discard old items(FIFO algorithm)"""
+                first_key = next(iter(self.cache_data))
+                del self.cache_data[first_key]
+                print("DISCARD: {}".format(first_key))
+
+    def get(self, key):
+        """ return the value of key in cache """
+        return self.cache_data.get(key)  
